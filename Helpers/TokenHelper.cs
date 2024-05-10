3bf61ea6b533da1525a07ebed955efe2867e7a6f
Gustavo Fring <gustavofring254@gmail.com>
2024-05-10 14:11:34 +0500

    v-4-api-clients

---
+++ Helpers/TokenHelper.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmazingFileVersionControl.ApiClients.Helpers
+{
+    public static class TokenHelper
+    {
+        public static string ExtractToken(string jsonResponse)
+        {
+            return JObject.Parse(jsonResponse)["token"].ToString();
+        }
+    }
+}
--- obj/Debug/net8.0/AmazingFileVersionControl.ApiClients.AssemblyInfo.cs
+++ obj/Debug/net8.0/AmazingFileVersionControl.ApiClients.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AmazingFileVersionControl.ApiClients")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b615d5370be359db35b23a34748f4c32d70d3150")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2971ed4e20aad3d6e063078824867ae948444697")]
 [assembly: System.Reflection.AssemblyProductAttribute("AmazingFileVersionControl.ApiClients")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AmazingFileVersionControl.ApiClients")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
